bfbf95dfa96f2922d6d19878f4da8090a59ad995
vijay05-sudo <vijaybisht270@gmail.com>
2020-10-29 10:33:35 +0530

    branch1 first commit

---
+++ EclipseProject/Project02/src/Customer.java
@@ -0,0 +1,9 @@
+
+public class Customer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("We have added Customer Project to Repo...!");
+	}
+
+}
